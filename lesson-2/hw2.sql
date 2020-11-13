7441cc8666889252386fee1985998cb915bfa02e
alexey-ana-smirnov <alexey.ana.smirnov@megafon.ru>
2020-11-13 14:19:39 +0400

    домашнее задание к уроку 2

---
+++ lesson-2/hw2.sql
@@ -0,0 +1,9 @@
+create database example;
+use example;
+
+create table users(
+id serial primary key, 
+name varchar(50)
+);
+
+create database sample;
---
+++ lesson-2/hw3.cmd
@@ -0,0 +1,2 @@
+mysqldump -u root -p example > "e:\Обучение\1 Базы данных\Урок 2\example_dump.sql"
+mysql -uroot -p sample < "e:\Обучение\1 Базы данных\Урок 2\HW\example_dump.sql"
